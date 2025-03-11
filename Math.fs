f31982f98316e469ce143b0ba2bf5c906541f5a9
widewoods <widewooz@gmail.com>
2025-03-11 10:07:21 +0900

    Change to uint64, add f abstraction

--- Math.fs
+++ Math.fs
@@ -5,12 +5,14 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let isEven x = x % 2L = 0L
+let isEven x = x % 2UL = 0UL
+
+let f n =
+  if isEven n then n/2UL
+  else 3UL*n + 1UL
 
 let collatz n =
   let rec iter n step = 
-    let n = int64 n
-    if n = 1L then step
-    elif isEven n then iter (n/2L) (step+1)
-    else iter (3L*n + 1L) (step + 1)
-  iter n 0
+    if n = 1UL then step
+    else iter (f n) (step + 1)
+  iter (uint64 n) 0

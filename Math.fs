a33695728b0d871d6c9a8a7bfa4e137905f32123
widewoods <widewooz@gmail.com>
2025-03-11 10:00:12 +0900

    Quiz2

--- Math.fs
+++ Math.fs
@@ -5,5 +5,12 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
+let isEven x = x % 2L = 0L
+
 let collatz n =
-  0
+  let rec iter n step = 
+    let n = int64 n
+    if n = 1L then step
+    elif isEven n then iter (n/2L) (step+1)
+    else iter (3L*n + 1L) (step + 1)
+  iter n 0
